6f847a919ce010781f00e7c51dd03d05a56c2383
Samuel Andreas Vývlek <151688126+Sam0xsk@users.noreply.github.com>
2025-12-21 20:23:03 +0100

    Update GalleryView.vue

--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -22,18 +22,18 @@ export default {
   data() {
   return {
     cars: [
-      { title: 'BMW E36 (pred a po úprave)', image: '/DriftLab/img/car1.jpg', link: 'https://www.youtube.com/watch?v=SEcwv4IFvoc' },
-      { title: 'Toyota GT86', image: '/DriftLab/img/car2.jpg', link: 'http://www.andyblackmoredesign.com/refreshed-toyota-86-x-fredric-aasbo/' },
-      { title: 'Nissan S13 240sx', image: '/DriftLab/img/car3.jpg', link: 'https://www.drifted.com/6-bad-ass-240sx-s13-drift-builds-on-drifted/' },
-      { title: 'Nissan 350Z', image: '/DriftLab/img/car4.jpg', link: 'https://stanceauto.co.uk/Nissan-350z' },
-      { title: 'Lexus IS 300', image: '/DriftLab/img/car5.png', link: 'https://www.clublexus.com/articles/pro-drifter-ken-gushi-builds-a-lexus-is300-thats-the-ultimate-street-car/' },
-      { title: 'Honda S2000', image: '/DriftLab/img/car6.png', link: 'https://www.reddit.com/r/NFSRides/comments/nu6kz3/honda_s2000_drift_build/' },
-      { title: 'Toyota Corolla (AE86)', image: '/DriftLab/img/car7.jpg', link: 'https://www.pexels.com/es-es/foto/coches-gente-vehiculo-vintage-16284885/' },
-      { title: 'Mazda RX-7', image: '/DriftLab/img/car8.jpg', link: 'https://www.pexels.com/es-es/foto/carretera-estacionado-aparcado-coche-negro-7862143/' },
-      { title: 'BMW E46', image: '/DriftLab/img/car9.jpg', link: 'https://www.pexels.com/es-es/foto/coche-vehiculo-capucha-capo-12330344/' },
-      { title: 'Mazda MX-5 Miata', image: '/DriftLab/img/car10.png', link: 'https://www.fastcar.co.uk/cars/mk1-mx-5-drift-car-minor-threat/' },
-      { title: 'Ford Mustang', image: '/DriftLab/img/car11.jpg', link: 'https://www.motoroso.com/FordSEMA/Project/5637d28df2ed7/Vaughn-Gitten-Jr.-Drift-Car/5637d4c776a2c/Vaughn-Gitten-Jrs-15-Mustang-Drift-Car' },
-      { title: 'Nissan Skyline R32', image: '/DriftLab/img/car12.jpg', link: 'https://www.peakpx.com/en/hd-wallpaper-desktop-fppzy' }
+      { title: 'BMW E36 (pred a po úprave)', image: '/public/img/car1.jpg', link: 'https://www.youtube.com/watch?v=SEcwv4IFvoc' },
+      { title: 'Toyota GT86', image: '/public/img/car2.jpg', link: 'http://www.andyblackmoredesign.com/refreshed-toyota-86-x-fredric-aasbo/' },
+      { title: 'Nissan S13 240sx', image: '/public/img/car3.jpg', link: 'https://www.drifted.com/6-bad-ass-240sx-s13-drift-builds-on-drifted/' },
+      { title: 'Nissan 350Z', image: '/public/img/car4.jpg', link: 'https://stanceauto.co.uk/Nissan-350z' },
+      { title: 'Lexus IS 300', image: '/public/img/car5.png', link: 'https://www.clublexus.com/articles/pro-drifter-ken-gushi-builds-a-lexus-is300-thats-the-ultimate-street-car/' },
+      { title: 'Honda S2000', image: '/public/img/car6.png', link: 'https://www.reddit.com/r/NFSRides/comments/nu6kz3/honda_s2000_drift_build/' },
+      { title: 'Toyota Corolla (AE86)', image: '/public/img/car7.jpg', link: 'https://www.pexels.com/es-es/foto/coches-gente-vehiculo-vintage-16284885/' },
+      { title: 'Mazda RX-7', image: '/public/img/car8.jpg', link: 'https://www.pexels.com/es-es/foto/carretera-estacionado-aparcado-coche-negro-7862143/' },
+      { title: 'BMW E46', image: '/public/img/car9.jpg', link: 'https://www.pexels.com/es-es/foto/coche-vehiculo-capucha-capo-12330344/' },
+      { title: 'Mazda MX-5 Miata', image: '/public/img/car10.png', link: 'https://www.fastcar.co.uk/cars/mk1-mx-5-drift-car-minor-threat/' },
+      { title: 'Ford Mustang', image: '/public/img/car11.jpg', link: 'https://www.motoroso.com/FordSEMA/Project/5637d28df2ed7/Vaughn-Gitten-Jr.-Drift-Car/5637d4c776a2c/Vaughn-Gitten-Jrs-15-Mustang-Drift-Car' },
+      { title: 'Nissan Skyline R32', image: '/public/img/car12.jpg', link: 'https://www.peakpx.com/en/hd-wallpaper-desktop-fppzy' }
     ]
   }
 }
